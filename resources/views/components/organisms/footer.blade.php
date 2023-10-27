43604dda719b20a425038f45646d676195501e72
Rifki Ardiansyah <rifkiardiansyah670@gmail.com>
2023-10-27 12:53:12 +0700

    email

--- resources/views/components/organisms/footer.blade.php
+++ resources/views/components/organisms/footer.blade.php
@@ -3,5 +3,9 @@
         <p class="text-center text-xs text-gray-600 font-semibold">
             &copy; Copyright 2023 PT. Kisah Cinta Anak Bangsa. All rights reserved.
         </p>
+        <p class="text-center text-xs text-gray-600 font-semibold">
+            Help & Support: <a href="mailto:support@bucin.co.id"
+                class="text-blue-600 cursor-pointer">support@bucin.co.id</a>
+        </p>
     </x-atoms.wrapper>
 </footer>
